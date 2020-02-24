2cafc74a1c2549c605a2784ec4cc70dbd43de292
Hugo Dornbierer <hdornbierer@gmail.com>
2020-02-24 15:54:06 +0100

    Implemented the User Genre Model

---
+++ db/migrate/20200224145012_create_user_genres.rb
@@ -0,0 +1,11 @@
+class CreateUserGenres < ActiveRecord::Migration[6.0]
+  def change
+    create_table :user_genres do |t|
+      t.references :user, null: false, foreign_key: true
+      t.references :genre, null: false, foreign_key: true
+      t.float :listening_percentage
+
+      t.timestamps
+    end
+  end
+end
