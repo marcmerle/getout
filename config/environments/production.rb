0ad72b9352084e08559a6744504be26c501c4464
Lomig <dunwael@gmail.com>
2020-02-24 16:01:09 +0100

    Fix typo - getour -> getout

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,5 @@
 Rails.application.configure do
-  config.action_mailer.default_url_options = { host: "https://getour.bar" }
+  config.action_mailer.default_url_options = { host: "https://getout.bar" }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
