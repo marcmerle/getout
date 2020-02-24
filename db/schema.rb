d761916d8192492a3ba76f059f308b7547f353fc
Claire Bauduin <bauduin.cla@gmail.com>
2020-02-24 15:35:50 +0100

    model place done

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_24_134641) do
+ActiveRecord::Schema.define(version: 2020_02_24_142844) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "places", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
+    t.float "latitude"
+    t.float "longitude"
+    t.string "type"
+    t.text "description"
+    t.time "opening_start"
+    t.time "opening_end"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
