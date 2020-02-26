63b25d8c839b38572faa966f35732cdee8afe33d
Hugo Dornbierer <hdornbierer@gmail.com>
2020-02-26 12:16:53 +0100

    last tweaks

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -4,7 +4,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def spotify
     @user = User.from_omniauth(request.env['omniauth.auth'])
     if @user.save
-      sign_in_and_redirect @user, event: :authentication # this will throw if @user is not activated
+      sign_in_and_redirect @user
       set_flash_message(:notice, :success, kind: 'Spotify') if is_navigational_format?
     else
       session['devise.spotify_data'] = request.env['omniauth.auth']
