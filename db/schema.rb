cbc64323c21825f82fff3b7fcaf44b477a8f0c33
Hugo Dornbierer <hdornbierer@gmail.com>
2020-02-27 21:12:33 +0100

    Schema update

--- db/schema.rb
+++ db/schema.rb
@@ -106,8 +106,6 @@ ActiveRecord::Schema.define(version: 2020_02_27_183627) do
     t.string "provider"
     t.string "uid"
     t.boolean "viewed_tastes_screen"
-    t.string "access_token"
-    t.string "refresh_token"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
