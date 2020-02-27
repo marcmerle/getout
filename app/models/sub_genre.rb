9862832c6806eabb53031e93b3f4e2488f72e716
Hugo Dornbierer <hdornbierer@gmail.com>
2020-02-27 22:17:41 +0100

    Tweaks

--- app/models/sub_genre.rb
+++ app/models/sub_genre.rb
@@ -5,19 +5,16 @@ class SubGenre < ApplicationRecord
   validates :name, uniqueness: { scope: [:genre_id] }
 
   def self.create_and_match(sub_genre)
-    puts sub_genre
     modified_sub_genre = sub_genre.gsub('hip hop', 'hip-hop')
     modified_sub_genre.gsub!('rap', 'hip-hop')
     modified_sub_genre.gsub!('thip-hop', 'trap')
     has_genre = false
     Genre.find_each do |genre|
-      puts "---------- try genre #{genre.name}"
       next unless modified_sub_genre.match?(genre.name)
+
       has_genre = true
-      puts "-----------------------match: #{has_genre}"
       create(name: sub_genre, genre: genre)
     end
-    puts has_genre
     create(name: sub_genre) unless has_genre
   end
 end
