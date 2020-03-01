b69387a925431c473c8cfa2e02eac12c4a9a41c7
Lomig <dunwael@gmail.com>
2020-03-01 16:56:55 +0100

    Using the new scope methods in the home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,31 +4,23 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
-    if current_user&.genres.present?
-      sql = <<-SQL
-        SELECT pg.place_id FROM user_genres AS ug
-        LEFT JOIN place_genres AS pg ON pg.genre_id = ug.genre_id
-        WHERE ug.user_id = #{current_user.id}
-        GROUP BY 1;
-      SQL
+    current_scope = policy_scope(Place)
+    current_scope = Place.policy_scope_by_genre(current_user, current_scope) if current_user&.genres.present?
 
-      results = ActiveRecord::Base.connection.execute(sql)
-      @places = Place.find(results.map { |x| x['place_id'] }).sample(8)
-    else
-      @places = Place.limit(8)
-    end
-
-    @places.each { |place| authorize(place) }
+    @places = current_scope.sample(8)
   end
 
   def tastes
     current_user.viewed_tastes_screen = true
 
+    sql = <<-SQL
+        LEFT JOIN user_genres
+          ON user_genres.genre_id = genres.id
+          AND user_genres.user_id = #{current_user.id}
+    SQL
+
     current_user.save
-    @genres = Genre
-              .all
-              .select('genres.*, user_genres.user_id')
-              .joins("left join user_genres on user_genres.genre_id = genres.id and user_genres.user_id = #{current_user.id}")
-              .order('user_genres.user_id, genres.name ASC')
+    @genres = Genre.all.select('genres.*, user_genres.user_id')
+                   .joins(sql).order('user_genres.user_id, genres.name ASC')
   end
 end
