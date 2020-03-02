126a31a8669fc6c8aa9930d8aa6b4908d4b1af7c
Hugo Dornbierer <hdornbierer@gmail.com>
2020-03-02 18:46:44 +0100

    Working JS, code needs to be refactored

---
+++ app/javascript/components/init_select2.js
@@ -0,0 +1,43 @@
+import $ from 'jquery';
+import 'select2';
+
+let i = 0
+
+const genresSearch = document.querySelector(".genres-search")
+
+const genres = genresSearch.dataset.genres.split(",")
+const colors = genresSearch.dataset.colors.split(",")
+
+const initSelect2 = () => {
+  $('#genres-search-bar-select').select2({ data: genres,
+                                           placeholder: "Enter a genre",
+                                           maximumSelectionLength: 60,
+                                           height: '56px'});
+};
+
+const genresForm = document.querySelector(".genres-search-form")
+
+const setColors = () => {
+  const choices = document.querySelectorAll(".select2-selection__choice");
+  const genresToForm = []
+  choices.forEach((choice) => {
+    const genreIndex = genres.indexOf(choice.getAttribute('title'));
+    const color = colors[genreIndex];
+    choice.setAttribute("style", `background-color: ${color}`)
+    genresToForm.push(choice.getAttribute('title'))
+  });
+  genresForm.elements.genres.value = genresToForm.join('+')
+  Rails.fire(genresForm, 'submit');
+
+}
+
+genresForm.addEventListener("click", (event) => {
+  const genresSelect = document.querySelector(".select2-results__options")
+
+  genresSelect.addEventListener('click', (event) => {
+    setColors();
+  });
+  setColors();
+});
+
+export { initSelect2 };
