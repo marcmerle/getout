f0b41fae4b26cd40cc59e909899b166b1bdaec8e
Hugo Dornbierer <hdornbierer@gmail.com>
2020-03-02 10:45:54 +0100

    Fix current-user on homepage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,17 +4,18 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
-    current_scope = policy_scope(Place)
-    current_scope = Place.policy_scope_by_genre(current_user, current_scope) if current_user&.genres.present?
+    if current_user
+      current_scope = policy_scope(Place)
+      current_scope = Place.policy_scope_by_genre(current_user, current_scope) if current_user.genres.present?
 
-    @places = current_scope.sample(8)
+      @places = current_scope.sample(8)
 
-    @artists = artists_events.sample(8)
-
-    # To be replaced by proper HTML 5 geolocation at some point
-    location = '52 ter Rue des Vinaigriers 75010 Paris'
-    current_scope = policy_scope(Place)
-    @places_nearby = Place.policy_scope_by_distance(location, current_scope).sample(8)
+      @artists = artists_events.sample(8)
+      # To be replaced by proper HTML 5 geolocation at some point
+      location = '52 ter Rue des Vinaigriers 75010 Paris'
+      current_scope = policy_scope(Place)
+      @places_nearby = Place.policy_scope_by_distance(location, current_scope).sample(8)
+    end
   end
 
   def tastes
